cc067d2fbd8984a04a06b2696ffe19a87aba84df
dgurduza <dmgurduza@edu.hse.ru>
2021-11-08 21:32:27 +0200

    fix main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ start_time = time.time()
 from extender import *
 #----------------------------------------------
 if __name__ == '__main__':
-    if sys.argv[1] == "-f" and len(sys.argv) == 4:
+    if len(sys.argv) > 1 and sys.argv[1] == "-f" and len(sys.argv) == 4:
         inputFileName = sys.argv[2]
         outputFileName = sys.argv[3]
         # Чтение исходного файла, содержащего данные, разделенные пробелами и переводами строки
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
         # Формирование массива строк, содержащего чистые данные в виде массива строк символов.
         strArray = str.replace("\n", "+=").split("+=")
-    elif sys.argv[1] == "-n" and len(sys.argv) == 4:
+    elif len(sys.argv) > 1 and sys.argv[1] == "-n" and len(sys.argv) == 4:
         size = int(sys.argv[2])
         se = Rnd(size)
         strArray = se.replace("\n", "+=").split("+=")
